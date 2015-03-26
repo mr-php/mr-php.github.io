ad7cc1f289c8edbcbc92a752129a5250fab12eee
Brett O'Donnell <cornernote@gmail.com>
2015-03-27 01:39:08 +1030

    Update mrphp.js

--- assets/js/mrphp.js
+++ assets/js/mrphp.js
@@ -1,7 +1,7 @@
 // force https
 if (window.location.protocol != 'https:') {
     alert(window.location.protocol);
-    window.location.protocol = 'https';
+    //window.location.protocol = 'https';
 }
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
