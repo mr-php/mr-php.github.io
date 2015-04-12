f2358f442911b08981bef0276e92a612a9f804d4
Brett O'Donnell <cornernote@gmail.com>
2015-04-12 10:24:24 +0930

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -18,7 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://getsimpleform.com/messages/ajax?form_api_token=3c63094a4813397862f3ad282bf8232b",
+                //url: "https://getsimpleform.com/messages/ajax?form_api_token=3c63094a4813397862f3ad282bf8232b",
+                url: "//formspree.io/hello@mrphp.com.au",
                 type: "post",
                 data: {
                     name: name,
