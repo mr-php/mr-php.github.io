3b5412f32b77fca0c46d8b4628916bd7350f92f5
Brett O'Donnell <cornernote@gmail.com>
2015-04-12 10:30:04 +0930

    change quotes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $('#contactForm').find("input,textarea").jqBootstrapValidation({
+    $('#contactForm').find('input,textarea').jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
@@ -10,20 +10,20 @@ $(function() {
             // reset errors
             $('#success').html('');
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
+            var name = $('input#name').val();
+            var email = $('input#email').val();
+            var phone = $('input#phone').val();
+            var message = $('textarea#message').val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                //url: "https://getsimpleform.com/messages/ajax?form_api_token=3c63094a4813397862f3ad282bf8232b",
-                url: "//formspree.io/hello@mrphp.com.au",
-                method: "POST",
-                dataType: "json",
+                //url: 'https://getsimpleform.com/messages/ajax?form_api_token=3c63094a4813397862f3ad282bf8232b',
+                url: '//formspree.io/hello@mrphp.com.au',
+                method: 'POST',
+                dataType: 'json',
                 data: {
                     name: name,
                     phone: phone,
@@ -33,37 +33,35 @@ $(function() {
                 cache: false,
                 success: function() {
                     // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
+                    $('#success').html('<div class="alert alert-success">');
+                    $('#success > .alert-success').html('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;')
+                        .append('</button>');
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append('<strong>Your message has been sent. </strong>');
                     $('#success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#contactForm').trigger('reset');
                 },
                 error: function() {
                     // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success').html('<div class="alert alert-danger">');
+                    $('#success > .alert-danger').html('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;')
+                        .append('</button>');
+                    $('#success > .alert-danger').append('<strong>Sorry ' + firstName + ', it seems that my mail server is not responding. Please try again later!');
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    //$('#contactForm').trigger("reset");
                 }
             })
         },
         filter: function() {
-            return $(this).is(":visible");
+            return $(this).is(':visible');
         }
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+    $('a[data-toggle="tab"]').click(function(e) {
         e.preventDefault();
-        $(this).tab("show");
+        $(this).tab('show');
     });
 });
 
