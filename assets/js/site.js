82075e800a0a69a088a45225c069b15df60fd06d
cornernote <cornernote@gmail.com>
2015-04-13 16:31:06 +0930

    merge more css/js files

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,9 @@
 ---
 ---
 
+{% include_relative _/jquery.min.js %}
+{% include_relative _/bootstrap.min.js %}
+{% include_relative _/jquery.easing.min.js %}
 {% include_relative _/jqBootstrapValidation.js %}
 {% include_relative _/contact.js %}
 {% include_relative _/navbar.js %}
