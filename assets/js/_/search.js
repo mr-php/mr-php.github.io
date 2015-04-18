c9829986d9094968ad40c32750f26ffb9656fc11
Brett O'Donnell <cornernote@gmail.com>
2015-04-18 12:11:21 +0930

    Update search.js

--- assets/js/_/search.js
+++ assets/js/_/search.js
@@ -5,22 +5,22 @@ e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
 _st('install','UfjNQb2D33csw9E3BFjv');
 
 $(function () {
-    $.getJSON("//api.swiftype.com/api/v1/public/engines/search.json?callback=?", {
-        engine_key: "5eMMdfkKCgz5wxyhR9RL", 
-        q: window.location.pathname.split(/[-/_]/).join(" "), 
+    $.getJSON('//api.swiftype.com/api/v1/public/engines/search.json?callback=?', {
+        engine_key: '5eMMdfkKCgz5wxyhR9RL', 
+        q: window.location.pathname.split(/[-/_]/).join(' ').trim(), 
         per_page: 10
     }).success(function (data) {
         console.log(data);
-        var $resultContainer = $("#st-results-container");
-        $resultContainer.html("");
+        var $resultContainer = $('#st-results-container');
+        $resultContainer.html('');
         if (data['records']['page'].length > 0) {
-            $resultContainer.append("<h2>Suggested pages based on this URL</h2>");
+            $resultContainer.append('<h2>Suggested pages based on this URL</h2>');
         }
         $.each(data['records']['page'], function(index, result) {
-            var resultHTML = "<p><a href='" + result['url'] + "'>" +
-            (result['highlight']['title'] || result['title']) + "</a><br>" +
+            var resultHTML = '<p><a href="' + result['url'] + '">' + 
+            (result['highlight']['title'] || result['title']) + '</a><br>' +
             (result['highlight']['body'] || result['body'].substring(0, 300)) +
-            "</p>";
+            '</p>';
             $resultContainer.append(resultHTML);
         });
     });
