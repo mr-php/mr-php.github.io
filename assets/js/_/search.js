bc69fdb1d300316f8aeed79276c238156a45ef10
Brett O'Donnell <cornernote@gmail.com>
2015-04-18 12:35:32 +0930

    Update search.js

--- assets/js/_/search.js
+++ assets/js/_/search.js
@@ -6,24 +6,26 @@
 //_st('install','pzPCqKnYUkavsQVNZryZ');
 
 // 404 search
-$(function () {
-    $.getJSON('//api.swiftype.com/api/v1/public/engines/search.json?callback=?', {
-        engine_key: 'pzPCqKnYUkavsQVNZryZ', 
-        q: window.location.pathname.split(/[-/_]/).join(' ').trim(), 
-        per_page: 10
-    }).success(function (data) {
-        console.log(data);
-        var $resultContainer = $('#st-results-container');
-        $resultContainer.html('');
-        if (data['records']['page'].length > 0) {
-            $resultContainer.append('<h2>Suggested pages based on this URL</h2>');
-        }
-        $.each(data['records']['page'], function(index, result) {
-            var resultHTML = '<hr><div class="post-preview"><a href="' + result['url'] + '"><h3>' + 
-            (result['highlight']['title'] || result['title']) + '</h3><p>' +
-            (result['highlight']['body'] || result['body'].substring(0, 300)) +
-            '</p></a></div>';
-            $resultContainer.append(resultHTML);
+if (page.path == '404.html') {
+    $(function () {
+        $.getJSON('//api.swiftype.com/api/v1/public/engines/search.json?callback=?', {
+            engine_key: 'pzPCqKnYUkavsQVNZryZ', 
+            q: window.location.pathname.split(/[-/_]/).join(' ').trim(), 
+            per_page: 10
+        }).success(function (data) {
+            console.log(data);
+            var $resultContainer = $('#st-results-container');
+            $resultContainer.html('');
+            if (data['records']['page'].length > 0) {
+                $resultContainer.append('<h2>Suggested pages based on this URL</h2>');
+            }
+            $.each(data['records']['page'], function(index, result) {
+                var resultHTML = '<hr><div class="post-preview"><a href="' + result['url'] + '"><h3>' + 
+                (result['highlight']['title'] || result['title']) + '</h3><p>' +
+                (result['highlight']['body'] || result['body'].substring(0, 300)) +
+                '</p></a></div>';
+                $resultContainer.append(resultHTML);
+            });
         });
     });
-});
+}
