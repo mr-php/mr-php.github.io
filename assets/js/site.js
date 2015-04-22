bda0416f944ecf84ec9708427d029d5c84cd2ccd
cornernote <cornernote@gmail.com>
2015-04-22 21:09:47 +0930

    more _/ to _includes/ and un-min bootstrap

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,12 @@
 ---
 ---
 
-{% include_relative _/jquery.min.js %}
-{% include_relative _/bootstrap.min.js %}
-{% include_relative _/jquery.lazyload.min.js %}
-{% include_relative _/jquery.easing.min.js %}
-{% include_relative _/jqBootstrapValidation.js %}
-{% include_relative _/navbar.js %}
-{% include_relative _/search.js %}
-{% include_relative _/contact.js %}
-{% include_relative _/main.js %}
+{% include_relative _includes/jquery.min.js %}
+{% include_relative _includes/bootstrap.min.js %}
+{% include_relative _includes/jquery.lazyload.min.js %}
+{% include_relative _includes/jquery.easing.min.js %}
+{% include_relative _includes/jqBootstrapValidation.js %}
+{% include_relative _includes/navbar.js %}
+{% include_relative _includes/search.js %}
+{% include_relative _includes/contact.js %}
+{% include_relative _includes/main.js %}
